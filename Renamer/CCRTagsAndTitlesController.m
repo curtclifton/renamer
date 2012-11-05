31ce32ff29b37bce63f2e2062f7c2d78d50a2290
Curt Clifton <curt.clifton@mac.com>
2012-11-04 21:17:45 -0800

    Added some test tags and titles until we start saving them.
    Implemented required data source methods for combo boxes.

--- Renamer/CCRTagsAndTitlesController.m
+++ Renamer/CCRTagsAndTitlesController.m
@@ -10,15 +10,24 @@
 
 @interface CCRTagsAndTitlesController ()
 @property (nonatomic, strong) NSMutableDictionary *tagsToArrayOfTitlesDictionary;
+
+- (NSArray *)_sortedTags;
 @end
 
 @implementation CCRTagsAndTitlesController
+
++ (NSDictionary *)_dictionaryForTesting;
+{
+    return @{@"regence" : @[@"privacy statement", @"explanation of benefits", @"expense ratio letter"], @"omni" : @[@"reimbursement", @"employment offer"], @"planet bike" : @[]};
+}
 
 - (id)init;
 {
     self = [super init];
     if (self) {
         _tagsToArrayOfTitlesDictionary = [NSMutableDictionary dictionary];
+        // CCC, 11/4/2012. For testing:
+        [_tagsToArrayOfTitlesDictionary addEntriesFromDictionary:[CCRTagsAndTitlesController _dictionaryForTesting]];
     }
     return self;
 }
@@ -27,29 +36,50 @@
 #pragma mark NSComboBoxDataSource
 - (NSInteger)numberOfItemsInComboBox:(NSComboBox *)aComboBox;
 {
-    // CCC, 11/4/2012. Implement.
-    return 1;
+    if (aComboBox == self.tagComboBox) {
+        return self.tagsToArrayOfTitlesDictionary.count;
+    } else if (aComboBox == self.titleComboBox) {
+        NSArray *titles = self.tagsToArrayOfTitlesDictionary[self.tagComboBox.stringValue];
+        if (titles == nil)
+            return 0;
+        else
+            return titles.count;
+    }
+
+    // During initial construction of the view hierarchy, we can get called before we have our combo box properties set, so…
+    return 0;
 }
 
 - (id)comboBox:(NSComboBox *)aComboBox objectValueForItemAtIndex:(NSInteger)index;
 {
-    // CCC, 11/4/2012.  implement.
-    return @"zero";
+    if (aComboBox == self.tagComboBox) {
+        return [self _sortedTags][index];
+    } else if (aComboBox == self.titleComboBox) {
+        NSArray *titles = self.tagsToArrayOfTitlesDictionary[self.tagComboBox.stringValue];
+        if (titles == nil)
+            return @"";
+        else
+            return titles[index];
+    }
+    
+    // During initial construction of the view hierarchy, we can get called before we have our combo box properties set, so…
+    return @"";
 }
 
-- (NSUInteger)comboBox:(NSComboBox *)aComboBox indexOfItemWithStringValue:(NSString *)string;
-{
-    // CCC, 11/4/2012. Implement.
-    if ([string isEqualToString:@"zero"])
-        return 0;
-    return NSNotFound;
-}
 
-- (NSString *)comboBox:(NSComboBox *)aComboBox completedString:(NSString *)string;
-{
-    // CCC, 11/4/2012. Implement.
-    return string;
-}
+//- (NSUInteger)comboBox:(NSComboBox *)aComboBox indexOfItemWithStringValue:(NSString *)string;
+//{
+//    // CCC, 11/4/2012. Implement.
+//    if ([string isEqualToString:@"zero"])
+//        return 0;
+//    return NSNotFound;
+//}
+//
+//- (NSString *)comboBox:(NSComboBox *)aComboBox completedString:(NSString *)string;
+//{
+//    // CCC, 11/4/2012. Implement.
+//    return string;
+//}
 
 #pragma mark NSComboBoxDelegate
 - (void)comboBoxSelectionDidChange:(NSNotification *)notification;
@@ -70,8 +100,14 @@
 - (void)clearFieldsAndRemember:(BOOL)remember;
 {
     // CCC, 11/4/2012. Clear tag also? Perhaps not. Seems like multiple in a row with the same tag are likely.
-    self.titleComboBox.stringValue = @"";
     // CCC, 11/4/2012. Remember.
+    self.titleComboBox.stringValue = @"";
+}
+
+#pragma mark - Private API
+- (NSArray *)_sortedTags;
+{
+    return [[self.tagsToArrayOfTitlesDictionary allKeys] sortedArrayUsingSelector:@selector(localizedCaseInsensitiveCompare:)];
 }
 
 @end
