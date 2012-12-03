b65b43f65c568d30d632394dc86c92df634f9f84
Curt Clifton <curt.clifton@mac.com>
2012-12-02 21:37:16 -0800

    Removed legacy outlet.

--- Renamer/CCRAppDelegate.h
+++ Renamer/CCRAppDelegate.h
@@ -21,8 +21,6 @@ extern NSString *CCRSourceBookmarksRestorationCoderKey;
 
 // bindings
 @property (nonatomic) BOOL enableControls;
-// CCC, 11/11/2012. Delete if you decide to remove the UI for showing this:
-@property (nonatomic, strong) NSURL *destinationDirectory;
 
 // main app window
 @property (assign) IBOutlet NSWindow *window;
